793cf23abd177e5e82bbc7c102fab44efb0ccf88
Ronan Collobert <ronan@collobert.com>
2015-03-01 20:00:16 -0800

    istype() honors __typename in metatable

--- env.lua
+++ env.lua
@@ -2,6 +2,13 @@ local env = {}
 
 -- user configurable function
 function env.istype(obj, typename)
+   local mt = getmetatable(obj)
+   if type(mt) == 'table' then
+      local objtype = rawget(mt, '__typename')
+      if objtype then
+         return objtype == typename
+      end
+   end
    return type(obj) == typename
 end
 
